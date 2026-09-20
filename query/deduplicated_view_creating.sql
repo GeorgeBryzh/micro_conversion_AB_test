68214a910bd6789d85ca81b8e8ccfa95bc544132
Georgiy Bryzhatiy <44099841+GeorgeBryzh@users.noreply.github.com>
2026-09-20 16:43:52 +0300

    Create deduplicated_logs view from dirty_event_logs

---
+++ query/deduplicated_view_creating.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE VIEW `round-cable-381420.cloud_solution.deduplicated_logs`
+AS
+ WITH flattened_logs AS (
+  SELECT
+      log_id,
+      TIMESTAMP(timestamp) AS event_timestamp,
+
+      JSON_EXTRACT_SCALAR(event_payload, '$.event_name') AS event_name,
+      JSON_EXTRACT_SCALAR(event_payload, '$.user_id') AS user_id,
+      JSON_EXTRACT_SCALAR(event_payload, '$.device.os') AS device_os,
+      JSON_EXTRACT_SCALAR(event_payload, '$.device.metadata.button_color') AS       
+      button_color
+    FROM `round-cable-381420.cloud_solution.dirty_event_logs`
+), 
+cleaned_logs AS (
+  SELECT log_id, 
+  event_timestamp, 
+  event_name, 
+  user_id, 
+  device_os, 
+  button_color,
+  ROW_NUMBER() OVER(
+      PARTITION BY user_id, event_name, event_timestamp 
+      ORDER BY log_id
+    ) as row_num
+  FROM flattened_logs
+)
+
+  SELECT log_id, 
+  event_timestamp, 
+  event_name, 
+  user_id, 
+  device_os, 
+  button_color,
+  LAG(event_timestamp) OVER(PARTITION BY user_id ORDER BY event_timestamp ASC) AS prev_timestamp
+  FROM cleaned_logs WHERE row_num = 1
