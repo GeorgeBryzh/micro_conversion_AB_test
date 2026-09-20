100a7c0fa41d9903a2d0c6cee2987001359a8dd7
Georgiy Bryzhatiy <44099841+GeorgeBryzh@users.noreply.github.com>
2026-09-20 16:57:41 +0300

    Create view for micro-conversion metrics

---
+++ query/cloud_click_trial_conversion.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE VIEW `round-cable-381420.cloud_solution.ab_click_trial_conversion` AS
+SELECT button_color, 
+device_os,
+COUNT(distinct user_id) AS total_users,
+COUNT(DISTINCT CASE WHEN event_name = 'click_trial' THEN user_id END) AS converted_users,
+ROUND(
+  SAFE_DIVIDE(
+      COUNT(DISTINCT CASE WHEN event_name = 'click_trial' THEN user_id END),
+      COUNT(DISTINCT user_id)
+  )*100, 2
+) AS conversion_rate_pct
+FROM `round-cable-381420.cloud_solution.deduplicated_logs` 
+WHERE event_name IN ('view_pricing', 'click_trial')
+GROUP BY button_color, device_os
+ORDER BY device_os asc;
