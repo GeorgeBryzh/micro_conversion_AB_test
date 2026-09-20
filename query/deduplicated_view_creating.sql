b84d85fadb865c4992df8ef24e13dd3ba28e3f3a
Georgiy Bryzhatiy <44099841+GeorgeBryzh@users.noreply.github.com>
2026-09-20 23:23:52 +0300

    Enhance deduplicated_logs view with comments
    Added comments to clarify the purpose of each section in the SQL view creation.

--- query/deduplicated_view_creating.sql
+++ query/deduplicated_view_creating.sql
@@ -1,6 +1,8 @@
 CREATE OR REPLACE VIEW `round-cable-381420.cloud_solution.deduplicated_logs`
 AS
+            
  WITH flattened_logs AS (
+-- Parse data from 'event_payload' column into separate columns
   SELECT
       log_id,
       TIMESTAMP(timestamp) AS event_timestamp,
@@ -12,7 +14,9 @@ AS
       button_color
     FROM `round-cable-381420.cloud_solution.dirty_event_logs`
 ), 
+
 cleaned_logs AS (
+-- Finds duplicate events partitioning by user_id, event_name, and event_timestamp
   SELECT log_id, 
   event_timestamp, 
   event_name, 
@@ -25,7 +29,7 @@ cleaned_logs AS (
     ) as row_num
   FROM flattened_logs
 )
-
+-- Deletes duplicate records where the row number > 1
   SELECT log_id, 
   event_timestamp, 
   event_name, 
@@ -34,3 +38,4 @@ cleaned_logs AS (
   button_color,
   LAG(event_timestamp) OVER(PARTITION BY user_id ORDER BY event_timestamp ASC) AS prev_timestamp
   FROM cleaned_logs WHERE row_num = 1
+-- The prev_timestamp column is used to calculate session numbers. See the 'cloud_user_session_creating' query for details
